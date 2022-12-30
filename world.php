739a6a2a80b4b368a2d2051a57fa15a99fb2f113
Zaria Chen Shui <zariachenshui@gmail.com>
2022-12-30 14:03:47 -0500

    Edited world.php and world.css to display country data within a table

--- world.php
+++ world.php
@@ -11,11 +11,25 @@ $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
 $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
 ?>
-<ul>
-<?php foreach ($results as $row): ?>
-  <li><?= $row['name'] . ' is ruled by ' . $row['head_of_state']; ?></li>
-<?php endforeach; ?>
-</ul>
 
-<?php 
-?>
+<table>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Continent</th>
+      <th>Independence</th>
+      <th>Head of State</th>
+    </tr>
+  </thead>
+
+  <tbody>
+    <?php foreach ($results as $row): ?>
+    <tr>
+      <td><?= $row['name'] ?></td>
+      <td><?= $row['continent'] ?></td>
+      <td><?= $row['independence_year'] ?></td>
+      <td><?= $row['head_of_state'] ?></td>
+    </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
