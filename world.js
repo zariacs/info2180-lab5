e45062d00e6164b5494f4fab0c96904eff8dca37
Zaria Chen Shui <zariachenshui@gmail.com>
2022-12-30 18:21:14 -0500

    Edited world.js, world.php, world.css and index.html to retrieve and display information for cities within a requested country. Edited index.html and world.css to add a footer to the page.

--- world.js
+++ world.js
@@ -1,19 +1,33 @@
-let btn = document.getElementById('lookup');
+let buttons = document.querySelectorAll('button');
 let result = document.getElementById('result');
 
-btn.onclick = (event) => {
-    event.preventDefault();
-    let search = document.getElementById("country").value;
-    let url = 'http://localhost/info2180-lab5/world.php?' + new URLSearchParams({ country: search });
+buttons.forEach(btn =>
+    btn.onclick = (event) => {
+        event.preventDefault();
+        let search = document.getElementById("country").value;
+        console.log(btn.id);
+        if (btn.id == 'lookup-country') {
+            var params = {
+                country: search,
+                lookup: 'country'
+            };
+        } else if (btn.id == 'lookup-cities') {
+            var params = {
+                country: search,
+                lookup: 'cities'
+            };
+        }
+        console.log(params);
+        let url = 'http://localhost/info2180-lab5/world.php?' + new URLSearchParams(params);
 
-    fetch(url)
-        .then(response => response.text())
-        .then(data => {
-            // Add country data to result div
-            result.innerHTML = data;
-        })
-        .catch(error => {
-            console.log(error);
-        })
-}
-
+        fetch(url)
+            .then(response => response.text())
+            .then(data => {
+                // Add country data to result div
+                result.innerHTML = data;
+            })
+            .catch(error => {
+                console.log(error);
+            })
+    }
+)
--- world.php
+++ world.php
@@ -4,32 +4,69 @@ $username = 'lab5_user';
 $password = 'password123';
 $dbname = 'world';
 $country = $_GET['country'];
+$lookup = $_GET['lookup'];
 
 $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-$stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
 
-$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+// Choose to display country or city info based on value of $lookup variable
 
-?>
+// Display coutnry info
+if ($lookup == 'country') { 
+  // Retrieve data from database using SQL query
+  $stmt = $conn->query("SELECT * FROM countries WHERE name LIKE '%$country%';");
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC); ?>
 
-<table>
-  <thead>
-    <tr>
-      <th>Name</th>
-      <th>Continent</th>
-      <th>Independence</th>
-      <th>Head of State</th>
-    </tr>
-  </thead>
+  <!-- Format results into HTML table -->
 
-  <tbody>
-    <?php foreach ($results as $row): ?>
-    <tr>
-      <td><?= $row['name'] ?></td>
-      <td><?= $row['continent'] ?></td>
-      <td><?= $row['independence_year'] ?></td>
-      <td><?= $row['head_of_state'] ?></td>
-    </tr>
-    <?php endforeach; ?>
-  </tbody>
-</table>
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>Continent</th>
+        <th>Independence</th>
+        <th>Head of State</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <?php foreach ($results as $row): ?>
+      <tr>
+        <td><?= $row['name'] ?></td>
+        <td><?= $row['continent'] ?></td>
+        <td><?= $row['independence_year'] ?></td>
+        <td><?= $row['head_of_state'] ?></td>
+      </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+ 
+<!-- Display city info for given country -->
+<?php } else if ($lookup == 'cities') { 
+  // Retrieve data from database using SQL query with join
+  $stmt = $conn->query("SELECT cities.name, cities.district, cities.population FROM cities JOIN countries ON countries.code=cities.country_code WHERE countries.code=(SELECT code FROM countries WHERE name='$country');");
+
+  $results = $stmt->fetchAll(PDO::FETCH_ASSOC); ?>
+
+  <!-- Format results into HTML table -->
+
+  <table>
+    <thead>
+      <tr>
+        <th>Name</th>
+        <th>District</th>
+        <th>Population</th>
+      </tr>
+    </thead>
+
+    <tbody>
+      <?php foreach ($results as $row): ?>
+      <tr>
+        <td><?= $row['name'] ?></td>
+        <td><?= $row['district'] ?></td>
+        <td><?= $row['population'] ?></td>
+      </tr>
+      <?php endforeach; ?>
+    </tbody>
+  </table>
+
+<?php } ?>
